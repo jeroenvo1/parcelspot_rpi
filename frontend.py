5ef85014581622754c673377c78c11be44ee9c0a
Rick Zegelaar <0920429@hr.nl>
2019-02-06 14:37:53 +0100

    Added light to openlocker route

--- frontend.py
+++ frontend.py
@@ -1,11 +1,18 @@
 import json
 
-from flask import Blueprint, render_template, flash, redirect, url_for, request, jsonify, session
+from flask import Blueprint, render_template, request, jsonify, session
 from imutils.video import VideoStream
 import requests
 from pyzbar import pyzbar
 import time
 import cv2
+import time
+import serial
+
+ser = serial.Serial(
+    port='/dev/ttyACM0',
+    baudrate=115200
+)
 
 frontend = Blueprint('frontend', __name__)
 
@@ -22,6 +29,7 @@ def pickup():
 
 @frontend.route('/send')
 def send():
+    # Get all the parcels that are associated with this user
     headers = {'Authorization' : session['token']}
     req = requests.get("http://142.93.224.133/api/parcel/", headers=headers)
     obj = json.loads(req.text)
@@ -35,9 +43,11 @@ def pickupdetail(id):
 
 @frontend.route('/scanned/', methods=['GET'])
 def scanned():
+    # Check if we detect a QRCCODE or a normal barcode
     if(session['barcode_type'] == "QRCODE"):
         req = requests.get("http://142.93.224.133/api/login/" + session['barcode'])
 
+        # Login our user with a QRCode
         if req.status_code == 201:
             session['user'] = req.json()['user']
             session['token'] = req.json()['token']
@@ -46,8 +56,8 @@ def scanned():
             return render_template('package.html', authorized=False)
     else:
 
+        # Fetch a parcel that's associated with te barcode if we can find it
         req = requests.get("http://142.93.224.133/api/parcel/delivery/" + session['barcode'])
-
         if req.status_code == 201:
             return render_template('process.html', package=req.json(), send=True)
         else:
@@ -56,27 +66,42 @@ def scanned():
 @frontend.route('/openlocker/<id>')
 def openlocker(id):
 
+    # Lampje aanzetten
+    for i in range(2000):
+        ser.write(id)
+
     # Do a call to  the ESP32
     return render_template('index.html')
 
 @frontend.route('/scanbarcode', methods=['GET','POST'])
 def barcode():
 
+    # When we get a GET request just show our index page
     if request.method == 'GET':
         return render_template('index.html')
 
+    # When a POST request arrives we kickoff the barcode scanning code
     if request.method == 'POST':
+        # Start up the camera
         vs = VideoStream(src=0).start()
+        # Wait till camera is initialized
         time.sleep(2.0)
         barcode_data = ""
         barcode_type = ""
         barcode_found = False
 
+        # Keep looping until we detect a barcode
         while True:
+            # Read a frame from our camera
             frame = vs.read()
+
+            # Make it grayscale for easier detection
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY, dstCn=0)
+
+            # Let pyzbar decode our image into barcode(s)
             barcodes = pyzbar.decode(frame)
 
+            # Loop through all the found barcodes
             for barcode in barcodes:
                 barcode_data = barcode.data.decode("utf-8")
                 barcode_type = barcode.type
@@ -86,8 +111,12 @@ def barcode():
             if barcode_found:
                 break
 
-        cv2.destroyAllWindows()
+        # Stop our camera
         vs.stop()
-        session['barcode'] = barcode_data;
-        session['barcode_type'] = barcode_type;
+
+        # Add the found type and data to our session
+        session['barcode'] = barcode_data
+        session['barcode_type'] = barcode_type
+
+        # Return a status for our client-side script
         return jsonify({'status': 'done'})
